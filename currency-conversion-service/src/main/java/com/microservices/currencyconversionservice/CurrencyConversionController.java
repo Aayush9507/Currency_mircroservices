518a7513e1ab77a29c47d888b71749d186eeb855
aayush goyal <aayushgoyal9507@gmail.com>
2019-07-09 17:38:39 -0600

    Using Feign client for service invocation

--- currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,9 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversionController {
+	
+	@Autowired
+	private CurrencyExchangeServiceProxy proxy;
 	
 	@GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversionBean convertCurreny( @PathVariable String from,
@@ -27,9 +31,17 @@ public class CurrencyConversionController {
 		
 		CurrencyConversionBean response = responseEntity.getBody();
 		
-//		System.out.println(new CurrencyConversionBean(response.getId(), from, to, 
-//				response.getConversionMultiple(), quantity, 
-//				quantity.multiply(response.getConversionMultiple()),response.getPort()));
+		return new CurrencyConversionBean(response.getId(), from, to, 
+				response.getConversionMultiple(), quantity, 
+				quantity.multiply(response.getConversionMultiple()),response.getPort());
+	}
+	
+	@GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversionBean convertCurrenyFeign( @PathVariable String from,
+			@PathVariable String to,
+			@PathVariable BigDecimal quantity) { 
+		
+		CurrencyConversionBean response = proxy.retrieveExchangeValue(from, to);
 		
 		return new CurrencyConversionBean(response.getId(), from, to, 
 				response.getConversionMultiple(), quantity, 
