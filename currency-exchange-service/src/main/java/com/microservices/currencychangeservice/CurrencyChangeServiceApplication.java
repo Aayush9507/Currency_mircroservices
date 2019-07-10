1a096bf771198585da4ed2b4e022eaa96461a996
aayush goyal <aayushgoyal9507@gmail.com>
2019-07-10 13:46:41 -0600

    Setting up Netflix Eureka naming server

--- currency-exchange-service/src/main/java/com/microservices/currencychangeservice/CurrencyChangeServiceApplication.java
+++ currency-exchange-service/src/main/java/com/microservices/currencychangeservice/CurrencyChangeServiceApplication.java
@@ -2,8 +2,10 @@ package com.microservices.currencychangeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CurrencyChangeServiceApplication {
 
 	public static void main(String[] args) {
